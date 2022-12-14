94391c8c433beb6d221838d0d69d725a127859a8
Mr. Tayyab Mughal <tayyabmughal676@gmail.com>
2022-12-14 18:18:21 +0500

    Initial Commit

---
+++ AImageGenerator/AImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AImageGeneratorApp.swift
+//  AImageGenerator
+//
+//  Created by Thor on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
